02f52c943f002d46831ecc35b219428a32f8d0a5
ortoct <104336410+ortoct@users.noreply.github.com>
2026-03-05 14:03:48 -0800

    added new Position

--- Ch13_AbstractClasses_Interfaces/Invader.java
+++ Ch13_AbstractClasses_Interfaces/Invader.java
@@ -5,6 +5,13 @@ public class Invader
    private int health, speedX, speedY;
    private Color color;
    private Position position;
-   private boolean isAlive;
-   private boolean[][] body;
+   private boolean isAlive, body[][];
+   private String matDir;
+   
+   public Invader( int x, int y )
+   {
+      this.position = new Position(x,y, Constants.ALIEN_WIDTH, Constants.ALIEN_HEIGHT);
+      this.color = Color.GREEN;
+      this.health = 1;
+   }
 }
