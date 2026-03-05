c228154bca6524f5bc4c4cc93940b911589c45b0
ortoct <104336410+ortoct@users.noreply.github.com>
2026-03-05 13:56:51 -0800

    Started Invader and added Position

---
+++ Ch13_AbstractClasses_Interfaces/Invader.java
@@ -0,0 +1,10 @@
+import javafx.scene.paint.Color;
+
+public class Invader
+{
+   private int health, speedX, speedY;
+   private Color color;
+   private Position position;
+   private boolean isAlive;
+   private boolean[][] body;
+}
