5806a07d1647e157a828a0f3d53736d13e086c32
ortoct <104336410+ortoct@users.noreply.github.com>
2026-03-24 14:12:31 -0700

    Videogame implements Comparable

---
+++ Ch19_Generics/Videogame.java
@@ -0,0 +1,35 @@
+import java.util.GregorianCalendar;
+
+public class Videogame implements Comparable<Videogame>
+{
+   //data fields
+   private String title, language, sDate;
+   private int copies;
+   private GregorianCalendar date;
+   //constructors
+   public Videogame(String title, String date, int copies, String language)
+   {
+      this.title = title;
+      this.sDate = date;
+      this.copies = copies;
+      this.language = language;
+      setDate();  //initializes this.date
+   }
+   //methods
+   public void setDate()
+   {
+      String[] tokens = this.sDate.split("/");
+      int month = Integer.parseInt(tokens[0]) - 1;
+      int day = Integer.parseInt( tokens[1] );
+      int year = Integer.parseInt( tokens[2] );
+      this.date = new GregorianCalendar(year, month, day);
+   }
+   
+   public int getCopies(){ return this.copies; }   
+   
+   @Override
+   public int compareTo( Videogame v )
+   {
+      return this.copies - v.getCopies();
+   }
+}
