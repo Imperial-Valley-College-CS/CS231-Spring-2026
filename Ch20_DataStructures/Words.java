d52aca42bb709d3541fe8d7a7be408d6937684da
ortoct <104336410+ortoct@users.noreply.github.com>
2026-04-30 14:27:37 -0700

    Finished Words.java

--- Ch20_DataStructures/Words.java
+++ Ch20_DataStructures/Words.java
@@ -1,4 +1,5 @@
 import java.util.TreeSet;
+import java.util.Collections;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -6,11 +7,11 @@ import java.io.FileNotFoundException;
 public class Words
 {
    public static String text = "";
-   public static TreeSet<String> words = new TreeSet<>();
+   public static TreeSet<String> words;
    
    public static void main(String[] args)
    {
-      readFile("TwinParadox.txt");
+      displayAscending("TwinParadox.txt", 1);      //0-ascending, 1-descending
    }
    
    public static boolean readFile( String filename )
@@ -19,16 +20,29 @@ public class Words
       {
          File f = new File(filename);
          Scanner scan = new Scanner(f);
-         System.out.println( scan.nextLine() );
+         text = scan.nextLine();
          return true;
       }catch( FileNotFoundException e )
       {
+         System.out.println( filename + " not found." );
          return false;
       }
    }
    
-   public static void displayAscending( String filename )
+   public static void displayAscending( String filename, int i )
    {
+      readFile(filename);
+      if( i == 1 )
+         words = new TreeSet<>(Collections.reverseOrder() );
+      else
+         words = new TreeSet<>(); 
+         
+      String[] tokens = text.split("\\W+");
+      for( String s : tokens )
+         words.add( s.toLowerCase() );
+         
+      while( !words.isEmpty() )
+         System.out.println( words.pollFirst() );
    }
    
    public static void displayDescending( String filename )
