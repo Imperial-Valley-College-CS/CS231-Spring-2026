d284ac1d646250b420fb587dcaee2231b157d0e2
ortoct <104336410+ortoct@users.noreply.github.com>
2026-04-30 14:02:03 -0700

    Wrote readFile method

--- Ch20_DataStructures/Words.java
+++ Ch20_DataStructures/Words.java
@@ -1,4 +1,7 @@
 import java.util.TreeSet;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Words
 {
@@ -7,11 +10,21 @@ public class Words
    
    public static void main(String[] args)
    {
+      readFile("TwinParadox.txt");
    }
    
    public static boolean readFile( String filename )
    {
-      return false;
+      try
+      {
+         File f = new File(filename);
+         Scanner scan = new Scanner(f);
+         System.out.println( scan.nextLine() );
+         return true;
+      }catch( FileNotFoundException e )
+      {
+         return false;
+      }
    }
    
    public static void displayAscending( String filename )
