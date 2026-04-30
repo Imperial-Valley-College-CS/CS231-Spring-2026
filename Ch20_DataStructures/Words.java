4593e4fb562072c499fd28167b81cdefc2071bf7
ortoct <104336410+ortoct@users.noreply.github.com>
2026-04-30 13:48:43 -0700

    Started Words.java

---
+++ Ch20_DataStructures/Words.java
@@ -0,0 +1,24 @@
+import java.util.TreeSet;
+
+public class Words
+{
+   public static String text = "";
+   public static TreeSet<String> words = new TreeSet<>();
+   
+   public static void main(String[] args)
+   {
+   }
+   
+   public static boolean readFile( String filename )
+   {
+      return false;
+   }
+   
+   public static void displayAscending( String filename )
+   {
+   }
+   
+   public static void displayDescending( String filename )
+   {
+   }
+}
